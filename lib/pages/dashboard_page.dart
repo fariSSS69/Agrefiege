9c838a383cad795e90c4e41e795c3d0c95d55367
Faris Maisonneuve <91095075+faris26@users.noreply.github.com>
2024-03-30 23:18:22 +0100

    Table formatting of the dashboard page

--- lib/pages/dashboard_page.dart
+++ lib/pages/dashboard_page.dart
@@ -13,6 +13,8 @@ class DashboardPage extends StatefulWidget {
 class _DashboardPageState extends State<DashboardPage> {
   late User _user;
   late List<Map<String, dynamic>> _observations;
+  bool _isAscending = true;
+  int _sortColumnIndex = 0;
 
   @override
   void initState() {
@@ -21,81 +23,109 @@ class _DashboardPageState extends State<DashboardPage> {
   }
 
   Future<void> _getUserData() async {
-  FirebaseAuth auth = FirebaseAuth.instance;
-  _user = auth.currentUser!;
-  final userSnapshot = await FirebaseFirestore.instance
-      .collection('Observateurs')
-      .where('email', isEqualTo: _user.email)
-      .get();
-
-  final userSnapshotData = userSnapshot.docs.first.data();
-  final lieux = userSnapshotData['Lieux'];
-
-  List<Map<String, dynamic>> allObservations = [];
-
-  for (final lieuRef in lieux) {
-    final lieuSnapshot = await lieuRef.get();
-    final parcellesSnapshot = await FirebaseFirestore.instance
-        .collection('Parcelles')
-        .where('Lieu', isEqualTo: lieuSnapshot.reference)
+    FirebaseAuth auth = FirebaseAuth.instance;
+    _user = auth.currentUser!;
+    final userSnapshot = await FirebaseFirestore.instance
+        .collection('Observateurs')
+        .where('email', isEqualTo: _user.email)
         .get();
 
-    for (final parcelleDoc in parcellesSnapshot.docs) {
-      final parcelleData = parcelleDoc.data();
-      final observationsSnapshot = await FirebaseFirestore.instance
-          .collection('Observations')
-          .where('Parcelle', isEqualTo: parcelleDoc.reference)
+    final userSnapshotData = userSnapshot.docs.first.data();
+    final lieux = userSnapshotData['Lieux'];
+
+    List<Map<String, dynamic>> allObservations = [];
+
+    for (final lieuRef in lieux) {
+      final lieuSnapshot = await lieuRef.get();
+      final parcellesSnapshot = await FirebaseFirestore.instance
+          .collection('Parcelles')
+          .where('Lieu', isEqualTo: lieuSnapshot.reference)
           .get();
 
-      final observations = observationsSnapshot.docs.map((doc) {
-        final observationData = doc.data();
-        return {
-          ...observationData,
-          'Parcelle': {'reference': observationData['Parcelle'], 'numero': parcelleData['Numero_parcelle']},
-        };
-      }).toList();
-      allObservations.addAll(observations);
+      for (final parcelleDoc in parcellesSnapshot.docs) {
+        final parcelleData = parcelleDoc.data();
+        final observationsSnapshot = await FirebaseFirestore.instance
+            .collection('Observations')
+            .where('Parcelle', isEqualTo: parcelleDoc.reference)
+            .get();
+
+        final observations = observationsSnapshot.docs.map((doc) {
+          final observationData = doc.data();
+          return {
+            ...observationData,
+            'Parcelle': {'reference': observationData['Parcelle'], 'numero': parcelleData['Numero_parcelle']},
+            'Date_observation': DateTime.fromMillisecondsSinceEpoch(observationData['Date_observation'].millisecondsSinceEpoch),
+          };
+        }).toList();
+        allObservations.addAll(observations);
+      }
     }
+
+    setState(() {
+      _observations = allObservations;
+    });
   }
 
-  setState(() {
-    _observations = allObservations;
-  });
-}
+  void _sort<T>(Comparable<T> getField(Map<String, dynamic> observation), int columnIndex, bool ascending) {
+    _observations.sort((a, b) {
+      final aValue = getField(a);
+      final bValue = getField(b);
 
+      return ascending
+          ? Comparable.compare(aValue, bValue)
+          : Comparable.compare(bValue, aValue);
+    });
+
+    setState(() {
+      _sortColumnIndex = columnIndex;
+      _isAscending = ascending;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Tableau de bord')),
       body: Center(
         child: _observations == null
             ? CircularProgressIndicator()
             : _observations.isEmpty
                 ? Text('Aucune observation trouvée.')
-                : ListView.builder(
-                    itemCount: _observations.length,
-                    itemBuilder: (context, index) {
-                      final observation = _observations[index];
-                      final date = (observation['Date_observation'] as Timestamp).toDate();
-                      final parcelle = observation['Parcelle'];
-                      final notation = observation['Notations'];
-                      final note = observation['Note'];
-                      return Card(
-                        child: ListTile(
-                          title: Text('Observation ${index + 1}'),
-                          subtitle: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text('Date: ${DateFormat('dd/MM/yyyy').format(date)}'),
-                              Text('Parcelle: ${observation['Parcelle']['numero']}'),
-                              Text('Notation: $notation'),
-                              Text('Note: $note'),
-                            ],
-                          ),
+                : SingleChildScrollView(
+                    scrollDirection: Axis.vertical,
+                    child: DataTable(
+                      sortColumnIndex: _sortColumnIndex,
+                      sortAscending: _isAscending,
+                      columns: [
+                        DataColumn(
+                          label: Text('Date'),
+                          onSort: (columnIndex, ascending) => _sort<DateTime>((observation) => observation['Date_observation'], columnIndex, ascending),
                         ),
-                      );
-                    },
+                        DataColumn(
+                          label: Text('Parcelle'),
+                          onSort: (columnIndex, ascending) => _sort<int>((observation) => observation['Parcelle']['numero'], columnIndex, ascending),
+                        ),
+                        DataColumn(
+                          label: Text('Notation'),
+                          onSort: (columnIndex, ascending) => _sort<String>((observation) => observation['Notations'], columnIndex, ascending),
+                        ),
+                        DataColumn(
+                          label: Text('Note'),
+                          onSort: (columnIndex, ascending) => _sort<String>((observation) => observation['Note'], columnIndex, ascending),
+                        ),
+                      ],
+                      rows: _observations
+                          .map(
+                            (observation) => DataRow(
+                              cells: [
+                                DataCell(Text(DateFormat('dd/MM/yyyy HH:mm').format(observation['Date_observation']))),
+                                DataCell(Text(observation['Parcelle']['numero'].toString())),
+                                DataCell(Text(observation['Notations'])),
+                                DataCell(Text(observation['Note'])),
+                              ],
+                            ),
+                          )
+                          .toList(),
+                    ),
                   ),
       ),
     );
