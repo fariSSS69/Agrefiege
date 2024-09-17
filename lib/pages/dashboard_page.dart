f453c1e7ea401597242b6b14a26987c086527609
Faris <faris.maisonneuve@gmail.com>
2024-09-17 15:57:22 +0200

    Ability to filter on location and date for admin and only date for other observers.

--- lib/pages/dashboard_page.dart
+++ lib/pages/dashboard_page.dart
@@ -18,8 +18,14 @@ class DashboardPage extends StatefulWidget {
 class _DashboardPageState extends State<DashboardPage> {
   late User _user;
   List<Map<String, dynamic>> _observations = [];
+  List<Map<String, dynamic>> _filteredObservations = [];
   bool _isAscending = true;
   int _sortColumnIndex = 0;
+
+  // Contrôleurs pour les filtres
+  TextEditingController _lieuController = TextEditingController();
+  DateTime? _startDate;
+  DateTime? _endDate;
 
   @override
   void initState() {
@@ -30,13 +36,11 @@ class _DashboardPageState extends State<DashboardPage> {
   Future<void> _getUserData() async {
     FirebaseAuth auth = FirebaseAuth.instance;
     _user = auth.currentUser!;
+    
+    List<Map<String, dynamic>> allObservations = [];
 
     if (_user.email == 'faris.maisonneuve@wanadoo.fr') {
-      List<Map<String, dynamic>> allObservations = [];
-
-      final lieuxSnapshot =
-          await FirebaseFirestore.instance.collection('Lieux').get();
-
+      final lieuxSnapshot = await FirebaseFirestore.instance.collection('Lieux').get();
       for (final lieuDoc in lieuxSnapshot.docs) {
         final parcellesSnapshot = await FirebaseFirestore.instance
             .collection('Parcelles')
@@ -66,10 +70,6 @@ class _DashboardPageState extends State<DashboardPage> {
           allObservations.addAll(observations);
         }
       }
-
-      setState(() {
-        _observations = allObservations;
-      });
     } else {
       final userSnapshot = await FirebaseFirestore.instance
           .collection('Observateurs')
@@ -78,8 +78,6 @@ class _DashboardPageState extends State<DashboardPage> {
 
       final userSnapshotData = userSnapshot.docs.first.data();
       final lieux = userSnapshotData['Lieux'];
-
-      List<Map<String, dynamic>> allObservations = [];
 
       for (final lieuRef in lieux) {
         final lieuSnapshot = await lieuRef.get();
@@ -111,16 +109,17 @@ class _DashboardPageState extends State<DashboardPage> {
           allObservations.addAll(observations);
         }
       }
-
-      setState(() {
-        _observations = allObservations;
-      });
     }
+
+    setState(() {
+      _observations = allObservations;
+      _filteredObservations = allObservations;
+    });
   }
 
   void _sort<T>(Comparable<T> Function(Map<String, dynamic> observation) getField,
       int columnIndex, bool ascending) {
-    _observations.sort((a, b) {
+    _filteredObservations.sort((a, b) {
       final aValue = getField(a);
       final bValue = getField(b);
 
@@ -135,15 +134,43 @@ class _DashboardPageState extends State<DashboardPage> {
     });
   }
 
+  void _filterObservations() {
+    List<Map<String, dynamic>> filtered = _observations;
+
+    if (_user.email == 'faris.maisonneuve@wanadoo.fr' && _lieuController.text.isNotEmpty) {
+      filtered = filtered.where((observation) => 
+        observation['Lieu'].toLowerCase().contains(_lieuController.text.toLowerCase())).toList();
+    }
+
+    if (_startDate != null && _endDate != null) {
+      filtered = filtered.where((observation) {
+        DateTime date = observation['Date_observation'];
+        return date.isAfter(_startDate!) && date.isBefore(_endDate!);
+      }).toList();
+    } else if (_startDate != null) {
+      filtered = filtered.where((observation) => 
+        observation['Date_observation'].isAfter(_startDate!)).toList();
+    } else if (_endDate != null) {
+      filtered = filtered.where((observation) => 
+        observation['Date_observation'].isBefore(_endDate!)).toList();
+    }
+
+    setState(() {
+      _filteredObservations = filtered;
+    });
+  }
+
   Future<void> _sendEmailWithAttachment(String filePath, BuildContext context) async {
-    String username = 'agrefiege@gmail.com'; // email de l'envoyeur
-    String password = 'emem bvrj qqea xbib'; // mdp application
+    final String email = _user.email ?? 'default@example.com'; // Email de l'utilisateur connecté
+
+    String username = 'agrefiege@gmail.com'; // Email de l'expéditeur
+    String password = 'emem bvrj qqea xbib'; // Mot de passe de l'application
 
     final smtpServer = gmail(username, password);
 
     final message = Message()
-      ..from = Address(username, 'Faris') // Le nom de l'envoyeur
-      ..recipients.add('faris.maisonneuve@gmail.com') // e-mail du destinataire
+      ..from = Address(username, 'Faris') // Nom de l'expéditeur
+      ..recipients.add(email) // Email du destinataire
       ..subject = 'Export des observations'
       ..text = 'Voici les observations exportées en pièce jointe.'
       ..attachments.add(FileAttachment(File(filePath))); // Attache le fichier exporté
@@ -152,7 +179,7 @@ class _DashboardPageState extends State<DashboardPage> {
       final sendReport = await send(message, smtpServer);
       print('Message envoyé : ' + sendReport.toString());
       ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-        content: Text("E-mail envoyé avec succès !"),
+        content: Text("E-mail envoyé avec succès à $email"),
       ));
     } on MailerException catch (e) {
       print('Échec de l\'envoi de l\'e-mail.');
@@ -171,7 +198,7 @@ class _DashboardPageState extends State<DashboardPage> {
       ['Date', 'Lieu', 'Parcelle', 'Notation', 'Note']
     ]; // Header
 
-    for (var observation in _observations) {
+    for (var observation in _filteredObservations) {
       rows.add([
         DateFormat('dd/MM/yyyy HH:mm').format(observation['Date_observation']),
         observation['Lieu'],
@@ -184,99 +211,175 @@ class _DashboardPageState extends State<DashboardPage> {
     final csvString = const ListToCsvConverter().convert(rows);
     final csvStringWithSemicolons = csvString.replaceAll(',', ';');
 
-    final Directory? directory = await getExternalStorageDirectory();
+    // Prépare le fichier CSV
+    final Directory? directory = await getTemporaryDirectory();
     final String directoryPath = directory!.path;
     final String filePath = '$directoryPath/observations_${DateTime.now()}.csv';
 
     final File file = File(filePath);
     await file.writeAsString(csvStringWithSemicolons);
 
-    // Appel de la fonction d'envoi d'e-mail
+    // Envoie l'email avec le fichier attaché
     await _sendEmailWithAttachment(filePath, context);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: _observations.isEmpty
-            ? const Text('Aucune observation trouvée.')
-            : SingleChildScrollView(
-                scrollDirection: Axis.vertical,
-                child: FittedBox(
-                  fit: BoxFit.fitWidth,
-                  child: DataTable(
-                    sortColumnIndex: _sortColumnIndex,
-                    sortAscending: _isAscending,
-                    columns: [
-                      DataColumn(
-                        label: const Text('Date'),
-                        onSort: (columnIndex, ascending) => _sort<DateTime>(
-                            (observation) => observation['Date_observation'],
-                            columnIndex,
-                            ascending),
-                      ),
-                      DataColumn(
-                        label: const Text('Lieu'),
-                        onSort: (columnIndex, ascending) => _sort<String>(
-                            (observation) => observation['Lieu'],
-                            columnIndex,
-                            ascending),
-                      ),
-                      DataColumn(
-                        label: const Text('Parcelle'),
-                        onSort: (columnIndex, ascending) => _sort<int>(
-                            (observation) => observation['Parcelle']['numero'],
-                            columnIndex,
-                            ascending),
-                      ),
-                      DataColumn(
-                        label: const Text('Notation'),
-                        onSort: (columnIndex, ascending) => _sort<String>(
-                            (observation) => observation['Notations'],
-                            columnIndex,
-                            ascending),
-                      ),
-                      DataColumn(
-                        label: const Text('Note'),
-                        onSort: (columnIndex, ascending) => _sort<String>(
-                            (observation) => observation['Note'],
-                            columnIndex,
-                            ascending),
-                      ),
-                    ],
-                    rows: _observations
-                        .map(
-                          (observation) => DataRow(
-                            cells: [
-                              DataCell(Text(DateFormat('dd/MM/yyyy HH:mm')
-                                  .format(observation['Date_observation']))),
-                              DataCell(Text(observation['Lieu'])),
-                              DataCell(Text(observation['Parcelle']['numero'].toString())),
-                              DataCell(Text(observation['Notations'])),
-                              DataCell(Text(observation['Note'])),
-                            ],
-                          ),
-                        )
-                        .toList(),
-                  ),
-                ),
-              ),
-      ),
       appBar: AppBar(
         centerTitle: true,
         actions: [
-          IconButton(
-            onPressed: _exportToExcel,
-            icon: Icon(Icons.file_download),
-            tooltip: 'Exporter vers Excel',
-          ),
+          if (_user.email != 'faris.maisonneuve@wanadoo.fr') ...[
+            IconButton(
+              onPressed: _exportToExcel,
+              icon: Icon(Icons.file_download),
+              tooltip: 'Exporter vers Excel',
+            ),
+          ],
+          if (_user.email == 'faris.maisonneuve@wanadoo.fr') ...[
+              IconButton(
+              onPressed: _exportToExcel,
+              icon: Icon(Icons.file_download),
+              tooltip: 'Exporter vers Excel',
+            ),
+          ],
           IconButton(
             onPressed: () {
               _getUserData();
             },
             icon: Icon(Icons.refresh),
             tooltip: 'Actualiser',
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          // Zone de filtrage
+          if (_user.email == 'faris.maisonneuve@wanadoo.fr') ...[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: _lieuController,
+                      decoration: InputDecoration(
+                        labelText: 'Filtrer par lieu',
+                      ),
+                      onChanged: (value) => _filterObservations(),
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                ],
+              ),
+            ),
+          ],
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                ElevatedButton(
+                  onPressed: () async {
+                    DateTime? pickedDate = await showDatePicker(
+                      context: context,
+                      initialDate: DateTime.now(),
+                      firstDate: DateTime(2000),
+                      lastDate: DateTime(2101),
+                    );
+                    if (pickedDate != null && pickedDate != _startDate) {
+                      setState(() {
+                        _startDate = pickedDate;
+                        _filterObservations();
+                      });
+                    }
+                  },
+                  child: Text(_startDate == null ? 'Choisir Date Début' : DateFormat('dd/MM/yyyy').format(_startDate!)),
+                ),
+                SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: () async {
+                    DateTime? pickedDate = await showDatePicker(
+                      context: context,
+                      initialDate: DateTime.now(),
+                      firstDate: DateTime(2000),
+                      lastDate: DateTime(2101),
+                    );
+                    if (pickedDate != null && pickedDate != _endDate) {
+                      setState(() {
+                        _endDate = pickedDate;
+                        _filterObservations();
+                      });
+                    }
+                  },
+                  child: Text(_endDate == null ? 'Choisir Date Fin' : DateFormat('dd/MM/yyyy').format(_endDate!)),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: _filteredObservations.isEmpty
+                ? const Text('Aucune observation trouvée.')
+                : SingleChildScrollView(
+                    scrollDirection: Axis.vertical,
+                    child: FittedBox(
+                      fit: BoxFit.fitWidth,
+                      child: DataTable(
+                        sortColumnIndex: _sortColumnIndex,
+                        sortAscending: _isAscending,
+                        columns: [
+                          DataColumn(
+                            label: const Text('Date'),
+                            onSort: (columnIndex, ascending) => _sort<DateTime>(
+                                (observation) => observation['Date_observation'],
+                                columnIndex,
+                                ascending),
+                          ),
+                          DataColumn(
+                            label: const Text('Lieu'),
+                            onSort: (columnIndex, ascending) => _sort<String>(
+                                (observation) => observation['Lieu'],
+                                columnIndex,
+                                ascending),
+                          ),
+                          DataColumn(
+                            label: const Text('Parcelle'),
+                            onSort: (columnIndex, ascending) => _sort<int>(
+                                (observation) => observation['Parcelle']['numero'],
+                                columnIndex,
+                                ascending),
+                          ),
+                          DataColumn(
+                            label: const Text('Notation'),
+                            onSort: (columnIndex, ascending) => _sort<String>(
+                                (observation) => observation['Notations'],
+                                columnIndex,
+                                ascending),
+                          ),
+                          DataColumn(
+                            label: const Text('Note'),
+                            onSort: (columnIndex, ascending) => _sort<String>(
+                                (observation) => observation['Note'],
+                                columnIndex,
+                                ascending),
+                          ),
+                        ],
+                        rows: _filteredObservations
+                            .map(
+                              (observation) => DataRow(
+                                cells: [
+                                  DataCell(Text(DateFormat('dd/MM/yyyy HH:mm')
+                                      .format(observation['Date_observation']))),
+                                  DataCell(Text(observation['Lieu'])),
+                                  DataCell(Text(observation['Parcelle']['numero'].toString())),
+                                  DataCell(Text(observation['Notations'])),
+                                  DataCell(Text(observation['Note'])),
+                                ],
+                              ),
+                            )
+                            .toList(),
+                      ),
+                    ),
+                  ),
           ),
         ],
       ),
